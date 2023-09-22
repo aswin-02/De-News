28977d58e56404ecd82aa657fd0627b2497ed6b9
aswin-02 <aswinvijay325@gmail.com>
2023-09-22 13:02:15 +0530

    updated

---
+++ src/Home.js
@@ -0,0 +1,96 @@
+function Home() {
+  return (
+    <div className="container ">
+      <div className="text-center">
+        <h1>LOGO</h1>
+      </div>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  World<span class="sr-only">(current)</span>
+                </a>
+              </li>
+             
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Trending
+                </a>
+                <div class="dropdown-menu " aria-labelledby="navbarDropdown" id="dropdown">
+                  <a class="dropdown-item text-white" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item text-white" href="#">
+                    Another action
+                  </a>
+                  {/* <div class="dropdown-divider"></div> */}
+                  <a class="dropdown-item text-white" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">
+                  Business
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">
+                  Fashion
+                </a>
+              </li>
+            <li>
+            <input
+                class="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              </li>
+              <li>
+              <button
+                class="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Search
+              </button>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+             
+            </form>
+          </div>
+        </nav>
+      </header>
+      <div className="container " id="flexbox">
+        <div id="slideshow"></div>
+
+        <footer id="footer"></footer>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
